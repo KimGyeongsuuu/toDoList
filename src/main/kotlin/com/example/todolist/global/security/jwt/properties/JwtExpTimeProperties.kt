dd7d84a9c8022ac2071418d56436933b10bd035a
KimGyeongsuuu <sk62628504@gmail.com>
2023-05-24 14:21:12 +0900

    :sparkles: JwtExpTimeProperties 추가

---
+++ src/main/kotlin/com/example/todolist/global/security/jwt/properties/JwtExpTimeProperties.kt
@@ -0,0 +1,11 @@
+package com.example.todolist.global.security.jwt.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties("jwt.time")
+class JwtExpTimeProperties(
+    val accessExp: Long,
+    val refreshExp: Long
+)
