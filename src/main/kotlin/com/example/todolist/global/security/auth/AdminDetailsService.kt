a40597eb9f034935e9f647d8412243fc8ab22188
KimGyeongsuuu <sk62628504@gmail.com>
2023-05-24 14:34:16 +0900

    :sparkles: member,admin details 생성

---
+++ src/main/kotlin/com/example/todolist/global/security/auth/AdminDetailsService.kt
@@ -0,0 +1,22 @@
+package com.example.todolist.global.security.auth
+
+import com.example.todolist.domain.member.common.exception.MemberNotFoundException
+import com.example.todolist.domain.member.entity.repository.MemberRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class AdminDetailsService(
+    private val memberRepository: MemberRepository
+): UserDetailsService {
+
+    @Transactional(readOnly = true, rollbackFor = [Exception::class])
+    override fun loadUserByUsername(username: String): UserDetails {
+        val admin = memberRepository.findByEmail(username)
+            ?: throw MemberNotFoundException()
+        return AdminDetails(admin.email)
+    }
+
+}
