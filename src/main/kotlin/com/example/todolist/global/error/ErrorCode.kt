7f5f86238132a3b615e428a090ab1c4e8aaaefcc
KimGyeongsuuu <sk62628504@gmail.com>
2023-05-28 21:05:33 +0900

    :sparkles: 예외처리 등록

--- src/main/kotlin/com/example/todolist/global/error/ErrorCode.kt
+++ src/main/kotlin/com/example/todolist/global/error/ErrorCode.kt
@@ -4,6 +4,7 @@ enum class ErrorCode(
     val message: String,
     val status: Int
 ) {
+
     // TOKEN
     EXPIRATION_TOKEN("토큰이 만료되었습니다.", 403),
     INVALID_TOKEN("토큰이 유효하지 않습니다.", 403),
@@ -13,5 +14,10 @@ enum class ErrorCode(
     MISMATCH_PASSWORD("비밀번호가 일치하지 않습니다.", 400),
 
     // MEMBER
-    MEMBER_NOT_FOUND("존재하지 않는 회원입니다.", 404);
+    MEMBER_NOT_FOUND("존재하지 않는 회원입니다.", 404),
+    NOT_VERIFY_MEMBER("인증되지 않은 회원입니다.", 401),
+
+    // TODOLIST
+    NOT_EXIST_TODOLIST("존재하지 않은 투두리스트입니다.", 404);
+
 }
