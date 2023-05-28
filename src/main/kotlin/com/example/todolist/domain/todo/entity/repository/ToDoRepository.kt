d22bf5a0c9c4e3e323d832ffcc10de967da859cc
KimGyeongsuuu <sk62628504@gmail.com>
2023-05-28 21:05:07 +0900

    :sparkles: todo상세보기 api

--- src/main/kotlin/com/example/todolist/domain/todo/entity/repository/ToDoRepository.kt
+++ src/main/kotlin/com/example/todolist/domain/todo/entity/repository/ToDoRepository.kt
@@ -4,4 +4,6 @@ import com.example.todolist.domain.todo.entity.ToDo
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
-interface ToDoRepository : JpaRepository<ToDo, UUID>
+interface ToDoRepository : JpaRepository<ToDo, UUID> {
+    fun findByIdx(idx: UUID) : ToDo?
+}
--- src/main/kotlin/com/example/todolist/domain/todo/presentation/ToDoController.kt
+++ src/main/kotlin/com/example/todolist/domain/todo/presentation/ToDoController.kt
@@ -2,20 +2,24 @@ package com.example.todolist.domain.todo.presentation
 
 import com.example.todolist.domain.todo.common.util.ToDoConverter
 import com.example.todolist.domain.todo.presentation.data.request.ToDoRequest
+import com.example.todolist.domain.todo.presentation.data.response.DetailToDoResponse
 import com.example.todolist.domain.todo.presentation.data.response.ToDoResponse
 import com.example.todolist.domain.todo.usecase.CreateToDoUseCase
+import com.example.todolist.domain.todo.usecase.GetDetailToDoUseCase
 import com.example.todolist.domain.todo.usecase.GetToDoUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.*
 import javax.validation.Valid
 
 @RestController
-@RequestMapping("/todolist")
+@RequestMapping("/todo")
 class ToDoController(
+    private val toDoConverter: ToDoConverter,
     private val createToDoUseCase: CreateToDoUseCase,
     private val getToDoUseCase: GetToDoUseCase,
-    private val toDoConverter: ToDoConverter,
+    private val getDetailToDoUseCase: GetDetailToDoUseCase
 ) {
 
     @PostMapping
@@ -29,5 +33,12 @@ class ToDoController(
         getToDoUseCase.execute()
             .map { toDoConverter.toToDoResponse(it,it.writer) }
             .let { ResponseEntity.ok(it) }
+
+    @GetMapping("/{idx}")
+    fun getDetailToDo(@PathVariable idx: UUID) : ResponseEntity<DetailToDoResponse> =
+        getDetailToDoUseCase.execute(idx)
+            .let { toDoConverter.toDetailToDoResponse(it,it.writer) }
+            .let { ResponseEntity.ok(it) }
+
 
 }
