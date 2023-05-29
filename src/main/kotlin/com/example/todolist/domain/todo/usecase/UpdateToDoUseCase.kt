9ce02563bcd4a9a44390e17f57da41d9b64dfb1a
KimGyeongsuuu <sk62628504@gmail.com>
2023-05-30 00:35:52 +0900

    :sparkles: ToDo 수정하기 UseCase 구현

---
+++ src/main/kotlin/com/example/todolist/domain/todo/usecase/UpdateToDoUseCase.kt
@@ -0,0 +1,33 @@
+package com.example.todolist.domain.todo.usecase
+
+import com.example.todolist.domain.auth.common.util.MemberUtil
+import com.example.todolist.domain.member.common.exception.NotVerifyMemberException
+import com.example.todolist.domain.todo.common.exception.NotExistToDoListException
+import com.example.todolist.domain.todo.entity.ToDo
+import com.example.todolist.domain.todo.entity.repository.ToDoRepository
+import com.example.todolist.domain.todo.usecase.dto.UpdateToDoDto
+import com.example.todolist.global.annotation.UseCaseWithTransaction
+import org.springframework.data.repository.findByIdOrNull
+import java.util.*
+
+@UseCaseWithTransaction
+class UpdateToDoUseCase(
+    private val toDoRepository: ToDoRepository,
+    private val memberUtil: MemberUtil
+) {
+
+    fun execute(idx: UUID, updateToDoDto: UpdateToDoDto) {
+        toDoRepository.findByIdOrNull(idx)
+            .let { it ?: throw NotExistToDoListException() }
+            .let { verifyMember(it) }
+            .let { it.updateToDo(updateToDoDto) }
+    }
+
+    private fun verifyMember(toDo: ToDo): ToDo {
+        if (!memberUtil.currentMember().equals(toDo.member)) {
+            throw NotVerifyMemberException()
+        }
+        return toDo
+    }
+
+}
