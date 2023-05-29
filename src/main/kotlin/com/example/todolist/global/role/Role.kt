d0eb5b3a7f6628e3e1a6f541881f6d649914d414
KimGyeongsuuu <sk62628504@gmail.com>
2023-05-29 22:22:06 +0900

    :recycle: 리턴 방법 변경

--- src/main/kotlin/com/example/todolist/global/role/Role.kt
+++ src/main/kotlin/com/example/todolist/global/role/Role.kt
@@ -5,8 +5,6 @@ import org.springframework.security.core.GrantedAuthority
 enum class Role : GrantedAuthority {
     ADMIN, MEMBER;
 
-    override fun getAuthority(): String {
-        return name
-    }
+    override fun getAuthority(): String = name
 
 }
