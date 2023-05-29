06b46ea70fd7f4576ad938d78e3d1f52f46d951c
KimGyeongsuuu <sk62628504@gmail.com>
2023-05-30 00:35:00 +0900

    :sparkles: upate 메서드 구현

--- src/main/kotlin/com/example/todolist/domain/todo/entity/ToDo.kt
+++ src/main/kotlin/com/example/todolist/domain/todo/entity/ToDo.kt
@@ -1,6 +1,7 @@
 package com.example.todolist.domain.todo.entity
 
 import com.example.todolist.domain.member.entity.Member
+import com.example.todolist.domain.todo.usecase.dto.UpdateToDoDto
 import com.example.todolist.global.entity.BaseUUIDEntity
 import java.time.LocalDate
 import java.util.*
@@ -11,12 +12,19 @@ class ToDo(
     @Column(name = "post_idx")
     override val idx: UUID,
     @Column(name = "title")
-    val title: String,
+    var title: String,
     @Column(name = "content")
-    val content: String,
+    var content: String,
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     val member: Member,
     @Column(name = "date")
     val date: LocalDate
-) : BaseUUIDEntity(idx)
+) : BaseUUIDEntity(idx) {
+
+    fun updateToDo(updateToDoDto: UpdateToDoDto) {
+        this.title = updateToDoDto.title
+        this.content = updateToDoDto.content
+    }
+
+}
