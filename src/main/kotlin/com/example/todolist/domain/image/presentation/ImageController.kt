ab481f71f5608a94c4e9069e84f3a9ea38832968
KimGyeongsuuu <sk62628504@gmail.com>
2023-06-02 10:03:37 +0900

    :sparkles: 이미지 업로드 api

---
+++ src/main/kotlin/com/example/todolist/domain/image/presentation/ImageController.kt
@@ -0,0 +1,22 @@
+package com.example.todolist.domain.image.presentation
+
+import com.example.todolist.domain.image.usecase.UploadImageUseCase
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+
+@RestController
+@RequestMapping("/image")
+class ImageController(
+    private val uploadImageUseCase: UploadImageUseCase
+) {
+
+    @PostMapping
+    fun uploadProfileImg(multipartFiles: List<MultipartFile>): ResponseEntity<List<String>> {
+        uploadImageUseCase.execute(multipartFiles)
+            .let { return ResponseEntity.ok(it) }
+    }
+
+}
