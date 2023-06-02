de786c1a5172ea80a224ab9aa36b330f5ebc3da4
KimGyeongsuuu <sk62628504@gmail.com>
2023-06-02 10:00:05 +0900

    :sparkles: AwsConfig 작성

---
+++ src/main/kotlin/com/example/todolist/global/aws/AwsConfig.kt
@@ -0,0 +1,33 @@
+package com.example.todolist.global.aws
+
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class AwsConfig {
+
+    @Value("\${cloud.aws.credentials.access-key}")
+    private lateinit var accessKey: String
+
+    @Value("\${cloud.aws.credentials.secret-key}")
+    private lateinit var secretKey: String
+
+    @Value("\${cloud.aws.region.static}")
+    private lateinit var region: String
+
+    @Bean
+    fun amazonS3(): AmazonS3 {
+        val awsCredentials: AWSCredentials = BasicAWSCredentials(accessKey, secretKey)
+        return AmazonS3ClientBuilder.standard()
+            .withRegion(region)
+            .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
+            .build()
+    }
+
+}
