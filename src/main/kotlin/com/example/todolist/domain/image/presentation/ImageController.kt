58848ccd06a0228f9f9357e0eee977b2c35919c5
KimGyeongsuuu <sk62628504@gmail.com>
2023-06-05 14:31:10 +0900

    :sparkles: 이미지 업데이트 api

--- src/main/kotlin/com/example/todolist/domain/image/presentation/ImageController.kt
+++ src/main/kotlin/com/example/todolist/domain/image/presentation/ImageController.kt
@@ -1,7 +1,10 @@
 package com.example.todolist.domain.image.presentation
 
+import com.example.todolist.domain.image.usecase.ImageUpdateUseCase
 import com.example.todolist.domain.image.usecase.ImageUploadUseCase
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,12 +13,18 @@ import org.springframework.web.multipart.MultipartFile
 @RestController
 @RequestMapping("/image")
 class ImageController(
-    private val imageUploadUseCase: ImageUploadUseCase
+    private val imageUploadUseCase: ImageUploadUseCase,
+    private val imageUpdateUseCase: ImageUpdateUseCase
 ) {
 
     @PostMapping
     fun uploadImage(multipartFile: MultipartFile): ResponseEntity<String> =
         imageUploadUseCase.execute(multipartFile)
             .let { ResponseEntity.ok(it) }
+
+    @PatchMapping
+    fun updateImage(multipartFile: MultipartFile): ResponseEntity<Void> =
+        imageUpdateUseCase.execute(multipartFile)
+            .let { ResponseEntity.status(HttpStatus.RESET_CONTENT).build() }
 
 }
