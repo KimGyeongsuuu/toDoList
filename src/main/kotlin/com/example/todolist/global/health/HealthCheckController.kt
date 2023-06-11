5c136671aa54043d77012f3e25ca0797194bdf18
KimGyeongsuuu <sk62628504@gmail.com>
2023-06-12 07:58:47 +0900

    :recycle: 띄어쓰기

--- src/main/kotlin/com/example/todolist/global/health/HealthCheckController.kt
+++ src/main/kotlin/com/example/todolist/global/health/HealthCheckController.kt
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthCheckController {
+
     @GetMapping("/health")
     fun healthCheck(): ResponseEntity<Map<String, String>> =
         ResponseEntity.ok(mapOf("message" to "ToDoList server running"))
